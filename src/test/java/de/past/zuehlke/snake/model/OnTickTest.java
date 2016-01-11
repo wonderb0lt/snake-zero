d45a8fde8dde8ad541b43bba803c62a10bb46c20
Patrick Stegmann <patrick.stegmann@zuehlke.com>
2016-01-11 15:52:02 +0100

    Added OnTick tests

---
+++ src/test/java/de/past/zuehlke/snake/model/OnTickTest.java
@@ -0,0 +1,35 @@
+package de.past.zuehlke.snake.model;
+
+import javafx.geometry.Point2D;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OnTickTest extends AbstractGameTest {
+    @Test
+    public void testSnakeAdvancesOnTick() {
+        SnakeGame game = prepareGameWithSnakePoints(Point2D.ZERO);
+        Snake snake = game.getPrimarySnake();
+        game.onTick();
+
+
+        Assert.assertEquals(1, snake.getPoints().size());
+        Assert.assertEquals(new Point2D(1, 0), snake.getPoints().getFirst());
+    }
+
+    @Test
+    public void foodIsSpawnedOnTick() {
+        SnakeGame game = prepareGameWithSnakePoints(Point2D.ZERO);
+
+        Assert.assertEquals("Game did not start off with no spawned food", 0, game.getSpawnedFood().size());
+        game.onTick();
+        Assert.assertEquals("Game did not spawn food after first tick", 1, game.getSpawnedFood().size());
+        game.onTick();
+        Assert.assertEquals("Food was removed after additional tick", 1, game.getSpawnedFood().size());
+
+        game.getSpawnedFood().clear();
+        Assert.assertEquals(0, game.getSpawnedFood().size());
+        game.onTick();
+        Assert.assertEquals("Food was not respawned after removal", 1, game.getSpawnedFood().size());
+
+    }
+}

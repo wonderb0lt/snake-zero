82740631fe0293cd4b50b79a983e987b10d0f734
Patrick Stegmann <patrick.stegmann@zuehlke.com>
2016-01-11 16:01:42 +0100

    Added simple direction change test

---
+++ src/test/java/de/past/zuehlke/snake/model/OnDirectionChangeTest.java
@@ -0,0 +1,14 @@
+package de.past.zuehlke.snake.model;
+
+import javafx.geometry.Point2D;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OnDirectionChangeTest extends AbstractGameTest {
+    @Test
+    public void testOnDirectionChange() {
+        SnakeGame game = prepareGameWithSnakePoints(Point2D.ZERO);
+        game.onDirectionChange(Direction.SOUTH);
+        Assert.assertEquals(Direction.SOUTH, game.getPrimarySnake().getCurrentDirection());
+    }
+}

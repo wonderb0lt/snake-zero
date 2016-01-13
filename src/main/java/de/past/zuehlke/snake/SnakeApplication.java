78f8ee66e1ab18e9da57038405190b4c9af2788c
Patrick Stegmann <patrick.stegmann@zuehlke.com>
2016-01-13 15:34:40 +0100

    Added configuration possiblity for looparound

--- src/main/java/de/past/zuehlke/snake/SnakeApplication.java
+++ src/main/java/de/past/zuehlke/snake/SnakeApplication.java
@@ -5,6 +5,7 @@ import de.past.zuehlke.snake.render.JavaFxSnakeGame;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -25,34 +26,38 @@ public class SnakeApplication extends Application {
         TextField tickSpeed = new TextField("175");
         TextField resolution = new TextField("400");
         TextField fieldSize = new TextField("10");
+        CheckBox loopAround = new CheckBox("Allow looparound?");
 
         Button startButton = new Button("Start");
         startButton.setOnAction(e -> {
             // Hide the config stage, play the game and show the config stage again when a round is done.
             primaryStage.hide();
-            startGame(new Stage(), configurationFromFields(winningPoints, tickSpeed, resolution, fieldSize));
+            startGame(new Stage(), configurationFromFields(winningPoints, tickSpeed, resolution, fieldSize, loopAround));
             primaryStage.show();
         });
 
 
+        int row = 0;
         gridPane.setHgap(5);
-        gridPane.addRow(0, new Label("Winning points: "), winningPoints);
-        gridPane.addRow(1, new Label("Tick speed (ms): "), tickSpeed);
-        gridPane.addRow(2, new Label("Resolution (px): "), resolution);
-        gridPane.addRow(3, new Label("Field size: "), fieldSize);
-        gridPane.addRow(4, startButton);
+        gridPane.setVgap(5);
+        gridPane.addRow(row++, new Label("Winning points: "), winningPoints);
+        gridPane.addRow(row++, new Label("Tick speed (ms): "), tickSpeed);
+        gridPane.addRow(row++, new Label("Resolution (px): "), resolution);
+        gridPane.addRow(row++, new Label("Field size: "), fieldSize);
+        gridPane.add(loopAround, 1, row++);
 
+        gridPane.addRow(row++, startButton);
         primaryStage.setScene(new Scene(gridPane));
         primaryStage.show();
     }
 
-    private SnakeConfiguration configurationFromFields(TextField winningPoints, TextField tickSpeed, TextField resolution, TextField fieldSize) {
+    private SnakeConfiguration configurationFromFields(TextField winningPoints, TextField tickSpeed, TextField resolution, TextField fieldSize, CheckBox loopAround) {
         SnakeConfiguration config = SnakeConfiguration.defaultConfiguration();
         config.setWinningPoints(Integer.parseInt(winningPoints.getText()));
         config.setTickSpeed(Integer.parseInt(tickSpeed.getText()));
         config.setResolution(Integer.parseInt(resolution.getText()));
         config.setFieldSize(Integer.parseInt(fieldSize.getText()));
-        config.setLoopAround(false);
+        config.setLoopAround(loopAround.isSelected());
 
         return config;
     }
